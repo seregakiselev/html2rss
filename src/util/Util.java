1493e893ff26865367e8c3636ccc0fdaca8a07c7
seregakiselev <seregakiselev>
2015-02-13 22:50:13 +0300

    Response encodings, response content size

--- src/util/Util.java
+++ src/util/Util.java
@@ -1,10 +1,12 @@
 package util;
 
+import rss.HtmlParser;
 import rss.RssBuilder;
 import rss.RssChannel;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
@@ -16,15 +18,28 @@ public class Util {
     public static void getXml(HttpServletRequest request, HttpServletResponse response) {
         try {
             String link = Misc.norm(request.getParameter("pageUrl"));
-            //TODO: parse site html
+            HtmlParser parser = new HtmlParser(link);
             RssBuilder builder = new RssBuilder();
-            RssChannel channel = new RssChannel();
+            RssChannel channel = parser.parse();
             channel.setLink(link).setLastBuildDate(new Date(System.currentTimeMillis()));
             builder.setRssChannel(channel);
-            PrintWriter out = response.getWriter();
-            out.println(builder.buildRss());
+            ByteArrayOutputStream stream = getRssOutputStream(builder);
+            response.setContentLength(stream.size());
+            response.setContentType("text/xml;charset=utf-8");
+            PrintWriter writer = response.getWriter();
+            writer.write(stream.toString());
+            writer.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    protected static ByteArrayOutputStream getRssOutputStream(RssBuilder builder)
+            throws IOException {
+        String body = builder.buildRss();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        stream.write(body.getBytes("UTF-8"));
+        stream.flush();
+        return stream;
     }
 }
